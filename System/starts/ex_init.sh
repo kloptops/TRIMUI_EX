371afbdb88299e1065b036ae7519f68aa6620557
Jacob <kloptops@gmail.com>
2024-04-01 19:22:59 +0800

    Added SFTPGO.

--- System/starts/ex_init.sh
+++ System/starts/ex_init.sh
@@ -2,6 +2,8 @@
 
 source /mnt/SDCARD/System/etc/ex_config
 
+mkdir -p ~/.config/
+
 if [[ "$NETWORK_FIX" == "Y" ]]; then
 
     if [ ! -f "$EX_CONFIG_PATH/mac_addr.conf" ]; then
@@ -29,7 +31,13 @@ if [[ "$NETWORK_SSH" == "Y" ]]; then
     mkdir -p /etc/dropbear
 
     # Currently broken
-    dropbear -R
+    # dropbear -R
+fi
+
+if [[ "$NETWORK_SFTPGO" == "Y" ]]; then
+    mkdir -p /opt/sftpgo
+
+    nice -2 /mnt/SDCARD/System/sftpgo/sftpgo serve -c /mnt/SDCARD/System/sftpgo/ > /dev/null &
 fi
 
 /mnt/SDCARD/System/bin/ex_update.sh
