ab6650504369f8814de9d4fb06ede1832de40a1e
Jacob <kloptops@gmail.com>
2024-04-01 17:15:14 +0800

    Added a bunch of missing libs required for ports.

--- System/starts/ex_init.sh
+++ System/starts/ex_init.sh
@@ -2,8 +2,6 @@
 
 source /mnt/SDCARD/System/etc/ex_config
 
-/mnt/SDCARD/System/bin/ex_update.sh
-
 if [[ "$NETWORK_FIX" == "Y" ]]; then
 
     if [ ! -f "$EX_CONFIG_PATH/mac_addr.conf" ]; then
@@ -11,7 +9,7 @@ if [[ "$NETWORK_FIX" == "Y" ]]; then
         bytes=$(dd if=/dev/urandom bs=6 count=1 2>/dev/null | xxd -p)
 
         # Format bytes into MAC address format
-        MAC_ADDR=$(echo "${bytes:0:2}:${bytes:2:2}:${bytes:4:2}:${bytes:6:2}:${bytes:8:2}:${bytes:10:2}")
+        MAC_ADDR=$(echo "00:00:${bytes:4:2}:${bytes:6:2}:${bytes:8:2}:${bytes:10:2}")
 
         # Output the random MAC address
         echo "MAC_ADDR=$MAC_ADDR" >> "$EX_CONFIG_PATH/mac_addr.conf"
@@ -22,9 +20,9 @@ if [[ "$NETWORK_FIX" == "Y" ]]; then
     ## MAC ADDRESS STUFF FROM: https://github.com/tGecko/TrimUI-Smart-Pro-resources?tab=readme-ov-file#startup-script
 
     ## Force a decent macaddr
-    ifconfig wlan0 down                        
-    ifconfig wlan0 hw ether "$MAC_ADDR"
-    ifconfig wlan0 up
+ifconfig wlan0 down
+ifconfig wlan0 hw ether "$MAC_ADDR"
+ifconfig wlan0 up
 fi
 
 if [[ "$NETWORK_SSH" == "Y" ]]; then
@@ -33,3 +31,5 @@ if [[ "$NETWORK_SSH" == "Y" ]]; then
     # Currently broken
     # dropbear -R
 fi
+
+/mnt/SDCARD/System/bin/ex_update.sh
