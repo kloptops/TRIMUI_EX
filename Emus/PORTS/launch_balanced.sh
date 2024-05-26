a3270af1781f68c929588a5114053b7d79f24b01
cobaltgit <cobaltsecuremail312@proton.me>
2024-05-26 20:35:18 +0100

    Add cpuswitch settings to all cpu profiles

--- Emus/PORTS/launch_balanced.sh
+++ Emus/PORTS/launch_balanced.sh
@@ -3,6 +3,10 @@
 source /mnt/SDCARD/System/etc/ex_config
 
 echo ondemand > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+echo 1 > /sys/devices/system/cpu/cpu0/online
+echo 1 > /sys/devices/system/cpu/cpu1/online
+echo 1 > /sys/devices/system/cpu/cpu2/online
+echo 1 > /sys/devices/system/cpu/cpu3/online
 echo 600000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
 echo 1200000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
 
